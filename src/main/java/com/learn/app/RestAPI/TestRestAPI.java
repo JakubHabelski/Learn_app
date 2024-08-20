7c9c685039d4f9c9860a3ccbe116b950811c584d
JakubHabelski <93724769+JakubHabelski@users.noreply.github.com>
2024-08-20 14:06:36 +0200

    20.08.2024 - finished lang detect and start login and mail validation

--- src/main/java/com/learn/app/RestAPI/TestRestAPI.java
+++ src/main/java/com/learn/app/RestAPI/TestRestAPI.java
@@ -1,15 +1,33 @@
 package com.learn.app.RestAPI;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.learn.app.Interfaces.UserInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestRestAPI {
+
+    @Autowired
+    UserInterface userInterface;
+
+
     @CrossOrigin(origins = "http://localhost:58547")
     @GetMapping("/testRestAPI")
-    public String test(){
-        return "Hello World";
+    public ResponseEntity test(@RequestParam String test_var){
+        boolean isTaken = userInterface.findByUserLogin(test_var) != null;
+        Map<String, String>  response= new HashMap<>();
+        response.put("message", test_var);
+        if(userInterface.findByUserLogin(test_var)==null){
+            response.put("message", "User not found");
+        } else {
+            response.put("message", "User found");
+        }
+
+        return ResponseEntity.ok(isTaken);
     }
 }
